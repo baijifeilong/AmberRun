34cb15c0f9c79337176c46f9bbf63ccd343aff52
BaiJiFeiLong <baijifeilong@gmail.com>
2016-09-20 13:18:13 +0800

    Add Android JUnit support

--- src/main/java/xyz/imamber/amberrun/MainActivity.kt
+++ src/main/java/xyz/imamber/amberrun/MainActivity.kt
@@ -1,8 +1,10 @@
-package bj.tian
+package xyz.imamber.amberrun
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.floatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +23,16 @@ class MainActivity : AppCompatActivity() {
             }
             button("Login") {
                 textSize = 30f
+                onClick {
+                    longToast("Clicked")
+                    text = "Logon"
+                    Snackbar.make(this, "A", Snackbar.LENGTH_LONG).show()
+                }
+            }
+            floatingActionButton {
+                onClick {
+                    Snackbar.make(this, "B", Snackbar.LENGTH_INDEFINITE).show()
+                }
             }
         }
     }
