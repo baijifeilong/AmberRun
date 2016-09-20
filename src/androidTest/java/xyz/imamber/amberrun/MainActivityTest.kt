f03cde88c0c3dc97ba8bfe0c9d3df46ae0ff9850
BaiJiFeiLong <baijifeilong@gmail.com>
2016-09-20 13:20:01 +0800

    Add MainActivityTest

---
+++ src/androidTest/java/xyz/imamber/amberrun/MainActivityTest.kt
@@ -0,0 +1,30 @@
+package xyz.imamber.amberrun
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.test.suitebuilder.annotation.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Author: BaiJiFeiLong@gmail.com
+ * DateTime: 2016/9/20 10:08
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+open class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun testOne() {
+        onView(withText("Login")).perform(click()).check(matches(withText("Logon")))
+    }
+}
