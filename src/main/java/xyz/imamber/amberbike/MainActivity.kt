66721ddac7a7e71ea059473411921aa20227e557
BaiJiFeiLong <baijifeilong@gmail.com>
2016-09-23 17:42:57 +0800

    Add dagger and logback

--- src/main/java/xyz/imamber/amberbike/MainActivity.kt
+++ src/main/java/xyz/imamber/amberbike/MainActivity.kt
@@ -1,39 +1,74 @@
 package xyz.imamber.amberbike
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
+import org.apache.commons.lang3.RandomUtils
 import org.jetbrains.anko.*
-import org.jetbrains.anko.design.floatingActionButton
+import org.slf4j.LoggerFactory
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val logger = LoggerFactory.getLogger(MainActivity::class.java)
+    lateinit var locationManager: LocationManager
+    val MOCK_PROVIDER = LocationManager.GPS_PROVIDER
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initLocationMock()
+
         verticalLayout {
             padding = dip(30)
-            editText {
-                hint = "Name"
-                textSize = 24f
-            }
-            editText {
-                hint = "Password"
-                textSize = 24f
-            }
-            button("Login") {
-                textSize = 30f
+            button("Mock location") {
                 onClick {
-                    longToast("Clicked")
-                    text = "Logon"
-                    Snackbar.make(this, "A", Snackbar.LENGTH_LONG).show()
-                }
-            }
-            floatingActionButton {
-                onClick {
-                    Snackbar.make(this, "B", Snackbar.LENGTH_INDEFINITE).show()
+                    toast("Clicked")
+                    logger.info("{}", "Clicked me")
                 }
             }
         }
+    }
+
+    private fun initLocationMock() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.addTestProvider(MOCK_PROVIDER, false, true, false, false, true, true, true, 0, 5);
+        locationManager.setTestProviderEnabled(MOCK_PROVIDER, true)
+        locationManager.requestLocationUpdates(MOCK_PROVIDER, 0L, 0F, object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                logger.info("Location changed: {}", location)
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+            }
+        })
+
+        Timer().schedule(object : TimerTask() {
+            @TargetApi(17)
+            override fun run() {
+                locationManager.setTestProviderLocation(MOCK_PROVIDER, Location(MOCK_PROVIDER).apply {
+                    time = System.currentTimeMillis()
+                    longitude = RandomUtils.nextDouble(110.0, 120.0)
+                    latitude = RandomUtils.nextDouble(30.0, 40.0)
+                    altitude = RandomUtils.nextDouble(10.0, 100.0)
+                    accuracy = 3.0f
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+                        elapsedRealtimeNanos = SystemClock.elapsedRealtimeNanos()
+                    }
+                })
+            }
+        }, 0, 5000)
     }
 }
